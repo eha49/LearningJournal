136ca5c0776ee881237e7d7f23b824b32023e59b
eha49 <habron.rasheed7@gmail.com>
2024-02-05 20:16:32 +0530

    Almost completed the mobile view

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const menuBar = document.getElementById('menu-bar');
+const hiddenPosts = document.getElementById('hidden-posts');
+const btnExpandPosts = document.getElementById('btn-expand-posts');
+
 document.body.addEventListener('click', handleClick);
 
 function handleClick(e) {
-    if (e.target.id) {
-        document.getElementById('menu-bar').classList.toggle('hidden');
+    if (e.target.id === 'menu-btn' | e.target.id === 'close-menu-btn') {
+        menuBar.classList.toggle('hidden');
     }
+
+    if (e.target.id === 'btn-expand-posts') {
+        hiddenPosts.classList.toggle('hidden');
+    }
+
+    (!hiddenPosts.classList.length) && (btnExpandPosts.textContent = 'Show Less');
+    
 }
